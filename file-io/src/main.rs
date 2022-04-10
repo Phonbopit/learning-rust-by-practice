bf73530ad5a4c248ac003bed6aa729f6204ed6a5
Chai Phonbopit <phonbopit@gmail.com>
2022-04-10 19:42:30 +0700

    handle error

--- file-io/src/main.rs
+++ file-io/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 struct Config {
     command: String,
@@ -7,11 +9,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // a &'static str will always be string literals that have the 'static lifetime.
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("Not enough argmuents");
+        }
         let command = args[1].clone();
         let filename = args[2].clone();
 
-        Config { command, filename }
+        Ok(Config { command, filename })
     }
 }
 
@@ -19,22 +25,35 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Args: {:?}", args);
 
-    let config: Config = Config::new(&args);
-    do_command(config);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments : {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = do_command(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    }
 }
 
-fn do_command(config: Config) {
+fn do_command(config: Config) -> Result<(), Box<dyn Error>> {
     let command = config.command;
     let filename = config.filename;
     if command == "read" {
         let content = fs::read_to_string(filename).expect("Can't read a file");
         println!("Read content : \n{}", content);
+        Ok(())
     } else if command == "write" {
         let new_content = "This is a new content to write in the file";
         fs::write(filename, new_content).expect("Can't write a file.");
         println!("Written!");
+        Ok(())
     } else if command == "del" {
         fs::remove_file(filename).expect("File can't delete.");
+        Ok(())
+    } else {
+        println!("No command found.");
+        Ok(())
     }
 }
 
