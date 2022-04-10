9d8e6c4306a6f5b0ef99287117b05d14ee1adb1d
Chai Phonbopit <phonbopit@gmail.com>
2022-04-10 19:17:27 +0700

    Update args handler

--- file-io/src/main.rs
+++ file-io/src/main.rs
@@ -1,17 +1,34 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    command: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let command = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { command, filename }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let command = &args[1];
-    let filename = &args[2];
-
     println!("Args: {:?}", args);
 
+    let config: Config = Config::new(&args);
+    do_command(config);
+}
+
+fn do_command(config: Config) {
+    let command = config.command;
+    let filename = config.filename;
     if command == "read" {
         let content = fs::read_to_string(filename).expect("Can't read a file");
-        println!("Read content : {}", content);
+        println!("Read content : \n{}", content);
     } else if command == "write" {
         let new_content = "This is a new content to write in the file";
         fs::write(filename, new_content).expect("Can't write a file.");
@@ -20,3 +37,12 @@ fn main() {
         fs::remove_file(filename).expect("File can't delete.");
     }
 }
+
+// fn parse_config(args: &[String]) -> Config {
+//     // Trade-ff using clone
+//     // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#the-trade-offs-of-using-clone
+//     let command = args[1].clone();
+//     let filename = args[2].clone();
+
+//     Config { command, filename }
+// }
